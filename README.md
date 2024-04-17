6d8471b5333f64871f28b95d4449e7b277cb9511
yo16 <xxyo1xx@gmail.com>
2024-04-17 14:58:09 +0900

    finished basic implementation

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # react_prac_react_dropzone
 try a module `react-dropzone`
+
+## Sites referred to
+- [react-dropzone](https://react-dropzone.js.org/)
+- [TypeScriptでreact-dropzoneを使ってドロップされたファイル名の表示](https://zenn.dev/e4exp/articles/8ac398cb4b1ae2)
+
--- prac_react_dropzone/src/App.tsx
+++ prac_react_dropzone/src/App.tsx
@@ -1,10 +1,30 @@
-import React from 'react';
+import { useCallback } from 'react';
+import { useDropzone } from 'react-dropzone';
+
 import './App.css';
 
 function App() {
+  const onDrop = useCallback((acceptedFiles: File[]) => {
+    // Do something!
+    console.log("Do something!!");
+    console.log(acceptedFiles);
+  }, []);
+
+  const {getRootProps, getInputProps, isDragActive} = useDropzone({onDrop});
+
+
   return (
     <>
       try react-dropzone!
+      <hr />
+      <div {...getRootProps()}>
+        <input {...getInputProps()} />
+        {
+          isDragActive?
+            <p>Drop the files here ...</p>:
+            <p>Drag 'n' drop some files here, or click to select files!</p>
+        }
+      </div>
     </>
   );
 }
